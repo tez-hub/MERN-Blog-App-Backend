a05f1e5ebc95cc0a0ffbbd24e40e141182623e76
tez-hub <dennismwema8@gmail.com>
2022-01-05 17:39:13 +0300

    update, delete and get user

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const authRoute = require("./routes/auth")
+const userRoute = require("./routes/users")
 
 dotenv.config();
 app.use(express.json());
@@ -13,6 +15,7 @@ mongoose.connect(process.env.MONGO_URL, {
 .catch((err) => console.log(err));
 
 app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
 
 app.listen("5000", () => {
     console.log("Backend is running")
